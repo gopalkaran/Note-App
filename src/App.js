2f040437e43a6cc02a97f829bef8157d238b9a93
Gopal <gopalkaran2021@gmail.com>
2021-04-11 23:28:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import styles from "./mycss.module.css";
 function App() {
+  const textStyle = {
+    backgroundColor : "green",
+    color : "white",
+    margin : "10px auto",
+    width : "600px"
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color:"red", backgroundColor:"green", margin:"auto" , width:"600px"}}>Hello</h1>
+      <h1 style={textStyle}>Gopal</h1>
+      <h1 className="text-style">Karan</h1>
+      <h1 className={styles.textStyleModule}>Where are you now?</h1>
+
     </div>
   );
 }
