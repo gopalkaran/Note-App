507aee5bc1faead40a68570cbf17072145c2c2b1
Gopal <gopalkaran2021@gmail.com>
2021-04-17 21:31:27 +0530

    created a multiplication program

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import './App.css';
-import Post from "./mycomponent/Post";
-import styles from "./mycss.module.css";
-import './mysass.scss';
+import React from "react";
+import Multiplier from "./mycomponent/Multiplier";
 function App() {
-  const textStyle = {
-    backgroundColor : "green",
-    color : "white",
-    margin : "10px auto",
-    width : "600px"
-  }
   return (
     <div className="App">
-      <h1 style={{color:"red", backgroundColor:"green", margin:"auto" , width:"600px"}}>Hello</h1> {/* INline CSS */}
-      <h1 style={textStyle}>Gopal</h1> {/* CSS using javascript object */}
-      <h1 className="text-style">Karan</h1> {/* CSS using App.css */}
-      <h1 className={styles.textStyleModule}>Where are you now?</h1> {/* CSS module*/}
-      <h1 className="textStyle">Kolkata</h1> {/* CSS using SaSS*/}
+      {/* <h1 style={{color:"red", backgroundColor:"green", margin:"auto" , width:"600px"}}>Hello</h1>
+      <h1 style={textStyle}>Gopal</h1>
+      <h1 className="text-style">Karan</h1>
+      <h1 className={styles.textStyleModule}>Where are you now?</h1>
+      <h1 className="textStyle">Kolkata</h1> */}
       {/* <Country/> */}
-      <Post/>
+      {/* <Post/> */}
+      {/* <MyTable /> */}
+      <Multiplier/>
     </div>
   );
 }
