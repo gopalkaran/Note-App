0b874ba3f27a8b3a090aee9b45ee8a8a88c43482
Gopal <gopalkaran2021@gmail.com>
2021-05-01 23:12:00 +0530

    viewnote styling is going

--- src/mycomponent/ViewNote.js
+++ src/mycomponent/ViewNote.js
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from "../css/ViewNote.module.css";
 
-function ViewNote({note}) {
+function ViewNote({ note }) {
   return (
-    <div>
-      <h2>{note.title}</h2>
-      <small>{note.description}</small>
+    <div className={styles.popup}>
+      <div className={styles.overlay}></div>
+      <div className={styles.content}>
+        <div className={styles.closebutton}>&times;</div>
+        <h1 className={styles.heading}>{note.title}</h1>
+        <small className={styles.des}>{note.description}</small>
+      </div>
     </div>
   );
 }
