12c1f5d2018bf3dea2485b5759a34f34a159c4ab
Gopal <gopalkaran2021@gmail.com>
2021-05-02 22:29:46 +0530

    viewnote styling is done using CSS

--- src/mycomponent/ViewNote.js
+++ src/mycomponent/ViewNote.js
@@ -1,14 +1,13 @@
 import React from "react";
-import styles from "../css/ViewNote.module.css";
+import styles from '../css/ViewNote.module.css';
 
 function ViewNote({ note }) {
   return (
-    <div className={styles.popup}>
-      <div className={styles.overlay}></div>
+    <div className={styles.bgmodal}>
       <div className={styles.content}>
-        <div className={styles.closebutton}>&times;</div>
-        <h1 className={styles.heading}>{note.title}</h1>
-        <small className={styles.des}>{note.description}</small>
+        <div className={styles.close}>+</div>
+        <h1>{note.title}</h1>
+        <p>{note.description}</p>
       </div>
     </div>
   );
